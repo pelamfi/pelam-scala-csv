671ccdaaaa9570d1daf967c69e572677e1cbab8d
Peter Lamberg <pgithub@pelam.fi>
2015-07-04 14:35:18 +0300

    Fix TODO (duplicated method)

--- src/main/scala/fi/pelam/csv/Table.scala
+++ src/main/scala/fi/pelam/csv/Table.scala
@@ -137,40 +137,15 @@ case class Table[RT, CT](charset: Charset,
     for (i <- 0 until colCount) yield CellKey(rowKey, i)
   }
 
-  // TODO: Use ones in CellTypes
   /**
    * Throws if the number of columns with given type is not 1
    */
-  def getSingleColByType(colType: CT): ColKey = {
-    val cols = colsByType(colType)
-    if (cols.size == 0) {
-      sys.error(s"Expected 1 column of type $colType but no columns of that type found.")
-    } else if (cols.size > 1) {
-      sys.error(s"Expected 1 column of type $colType but more than 1 found.")
-    } else {
-      cols(0)
-    }
-  }
+  def getSingleColByType(colType: CT) = cellTypes.getSingleColByType(colType)
 
   /**
    * Throws if the number of rows with given type is not 1
    */
-  def getSingleRowByType(rowType: RT): RowKey = {
-    val rows = rowsByType(rowType)
-
-    if (rows.size == 0) {
-
-      sys.error(s"Expected 1 row of type $rowType but no rows of that type found.")
-
-    } else if (rows.size > 1) {
-
-      sys.error(s"Expected 1 row of type $rowType but more than 1 found. " +
-        rows.tail.foldLeft(rows.head.toString)(_ + ", " + _))
-
-    } else {
-      rows(0)
-    }
-  }
+  def getSingleRowByType(rowType: RT) = cellTypes.getSingleRowByType(rowType)
 
   /**
    * Get cells from single column of colType for each row of rowType.
