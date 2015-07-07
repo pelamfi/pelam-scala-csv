b5021350223b2f0889beeb9331c2f53fb15a7aa4
Peter Lamberg <pgithub@pelam.fi>
2015-07-07 08:51:42 +0300

    Javadoc for CsvConstants.

--- src/main/scala/fi/pelam/csv/CsvConstants.scala
+++ src/main/scala/fi/pelam/csv/CsvConstants.scala
@@ -2,12 +2,30 @@ package fi.pelam.csv
 
 object CsvConstants {
 
+  /**
+   * Default separator in CSV is the comma as in "Comma Separated Values".
+   *
+   * However this canges depending on locale. Comma is not used in (some?)
+   * locales where standard number formats use comma as the decimal separator.
+   *
+   * At least MS Excel with Finnish locale uses semicolon as separator.
+   */
   val defaultSeparatorChar: Char = ','
 
+  /**
+   * Excel uses double quotes for cells containing the current separator character.
+   * Two double quotes are used to encode a doulbe quote character in cell.
+   */
   val quoteChar: Char = '"'
 
+  /**
+   * Double quote as string.
+   */
   val quote: String = quoteChar.toString
 
+  /**
+   * Quote char escaped as two quotes.
+   */
   val doubleQuote: String = quote + quote
 
 }
