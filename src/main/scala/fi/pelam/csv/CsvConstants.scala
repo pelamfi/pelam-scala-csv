2bf9e749ae4ed6661329b327b10c9e9d18716240
Peter Lamberg <pgithub@pelam.fi>
2015-07-28 13:48:57 +0300

    Fix typo.

--- src/main/scala/fi/pelam/csv/CsvConstants.scala
+++ src/main/scala/fi/pelam/csv/CsvConstants.scala
@@ -1,11 +1,13 @@
 package fi.pelam.csv
 
+import java.nio.charset.StandardCharsets
+
 object CsvConstants {
 
   /**
    * Default separator in CSV is the comma as in "Comma Separated Values".
    *
-   * However this canges depending on locale. Comma is not used in (some?)
+   * However this changes depending on locale. Comma is not used in (some?)
    * locales where standard number formats use comma as the decimal separator.
    *
    * At least MS Excel with Finnish locale uses semicolon as separator.
@@ -27,5 +29,11 @@ object CsvConstants {
    * Quote char escaped as two quotes.
    */
   val doubleQuote: String = quote + quote
+
+  /**
+   * There is no really any de facto character set for CSV, but UTF-8 is
+   * a good bet for anything.
+   */
+  val defaultCharset = StandardCharsets.UTF_8
 
 }
