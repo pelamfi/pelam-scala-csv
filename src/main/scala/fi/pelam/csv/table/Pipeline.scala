c0e8f9ff05c8464edfde4c0b136281ed879aa3ba
Peter Lamberg <pgithub@pelam.fi>
2015-07-30 17:29:48 +0300

    Cleanup Pipeline a bit.

--- src/main/scala/fi/pelam/csv/table/Pipeline.scala
+++ src/main/scala/fi/pelam/csv/table/Pipeline.scala
@@ -7,30 +7,30 @@ sealed trait Pipeline[S <: Success] {
 }
 
 object Pipeline {
-
   case class Stage[S <: Success](phaseFunc: S => S) extends Pipeline[S] {
-    def map(mapFunc: S => S) = Stage[S](state => mapFunc(phaseFunc(state)))
+    override def map(mapFunc: S => S) = Stage[S](state => mapFunc(phaseFunc(state)))
 
-    def flatMap(inner: S => Pipeline[S]): Pipeline[S] = FlatmapStage(this, inner)
+    override def flatMap(inner: S => Pipeline[S]): Pipeline[S] = FlatmapStage(this, inner)
 
-    def run(inputState: S) = phaseFunc(inputState)
+    override def run(inputState: S) = phaseFunc(inputState)
   }
 
   case class FlatmapStage[S <: Success](outer: Pipeline[S],
     inner: S => Pipeline[S],
     mapFunc: S => S = (state: S) => state) extends Pipeline[S] {
 
-    def map(newMapFunc: S => S) = FlatmapStage(outer, inner,
+    override def map(newMapFunc: S => S) = FlatmapStage(outer, inner,
       (state: S) => mapFunc(newMapFunc(state)))
 
-    def flatMap(newInner: S => Pipeline[S]): Pipeline[S] = FlatmapStage(this, newInner)
+    override def flatMap(newInner: S => Pipeline[S]): Pipeline[S] = FlatmapStage(this, newInner)
 
-    def run(inputState: S) = {
+    override def run(inputState: S) = {
       val outerResult = outer.run(inputState)
       if (outerResult.isSuccess) {
-        mapFunc(inner(outerResult).run(outerResult)) // to call the inner func, we need state and we get the wrapper,
+        // Call the inner (and downstream in the pipeline) stages
+        mapFunc(inner(outerResult).run(outerResult))
       } else {
-        // Don't run further phases if one had error
+        // Don't run further stages if one had error
         mapFunc(outerResult)
       }
     }
