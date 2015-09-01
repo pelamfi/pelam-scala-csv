1ea2bc9fd4829056f350f4095b7ac222fb279da4
Peter Lamberg <pgithub@pelam.fi>
2015-09-01 12:50:54 +0300

    More specific tests for TableReaderEvaluator.

--- src/test/scala/fi/pelam/csv/table/TableReaderEvaluatorTest.scala
+++ src/test/scala/fi/pelam/csv/table/TableReaderEvaluatorTest.scala
@@ -34,10 +34,66 @@ import fi.pelam.csv.table.TestColType._
 import fi.pelam.csv.table.TestRowType._
 import org.junit.Assert._
 import org.junit.Test
+import org.mockito.Matchers._
+import org.mockito.Mockito.{verify, when, times}
+import org.scalatest.mock.MockitoSugar._
 
 class TableReaderEvaluatorTest {
   import TableReaderTest._
   import TableReaderEvaluatorTest._
+
+  val mockReader = mock[TableReader[TestRowType, TestColType, SimpleMetadata]]
+
+  val table1 = Table[TestRowType, TestColType, SimpleMetadata](
+    IndexedSeq(StringCell(CellKey(0,0), "foocell")))
+
+  val table2 = Table[TestRowType, TestColType, SimpleMetadata](
+    IndexedSeq(StringCell(CellKey(1,0), "barcell")))
+
+  val noErrors = TableReadingErrors()
+
+  val error1 = TableReadingError("foo")
+
+  val error2 = TableReadingError("bar")
+
+  val badErrors = TableReadingErrors(2, IndexedSeq(error1))
+
+  val worseErrorsSameStage = TableReadingErrors(2, IndexedSeq(error1, error2))
+
+  val worseErrorsEarlierStage = TableReadingErrors(1, IndexedSeq(error1))
+
+  @Test
+  def testBetterThanInitialResult: Unit = {
+    val initialEvaluator = TableReaderEvaluator[TestRowType, TestColType, SimpleMetadata]()
+
+    when(mockReader.read()).thenReturn((table1, worseErrorsSameStage))
+
+    val finalEvaluator = initialEvaluator.evaluateReader(mockReader)
+
+    assertEquals(Some((table1, worseErrorsSameStage)), finalEvaluator.result)
+  }
+
+
+  @Test
+  def testSecondWorseResult: Unit = {
+    val initialEvaluator = TableReaderEvaluator[TestRowType, TestColType, SimpleMetadata]()
+
+    when(mockReader.read()).thenReturn((table1, badErrors))
+
+    val secondEvaluator = initialEvaluator.evaluateReader(mockReader)
+
+    verify(mockReader, times(1)).read()
+
+    // This is not even evaluated
+    when(mockReader.read()).thenReturn((table2, worseErrorsSameStage))
+
+    verify(mockReader, times(1)).read()
+
+    val finalEvaluator = secondEvaluator.evaluateReader(mockReader)
+
+    assertEquals(Some((table1, badErrors)), finalEvaluator.result)
+
+  }
 
   @Test
   def testDetectSeparatorAndCharset {
