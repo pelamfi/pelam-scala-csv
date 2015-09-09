9eec2d5fda243656b368b154b140393aa6fd482b
Peter Lamberg <pgithub@pelam.fi>
2015-09-09 08:48:00 +0300

    Fix TODO.

--- src/main/scala/fi/pelam/csv/table/TableWriter.scala
+++ src/main/scala/fi/pelam/csv/table/TableWriter.scala
@@ -26,22 +26,22 @@ import fi.pelam.csv.stream.CsvWriter
 class TableWriter[RT, CT, M <: TableMetadata](table: Table[RT, CT, M]) {
 
   def write(output: java.io.OutputStream) = {
+    try {
+      val writer = new OutputStreamWriter(output, table.metadata.charset)
 
-    val writer = new OutputStreamWriter(output, table.metadata.charset)
+      val csvWriter = new CsvWriter(writer, table.metadata.separator)
 
-    val csvWriter = new CsvWriter(writer, table.metadata.separator)
+      val cells = table.getCells()
 
-    val cells = table.getCells()
+      csvWriter.write(cells)
 
-    csvWriter.write(cells)
+      // Add the final line end
+      csvWriter.goToNextRow()
 
-    // Add the final line end
-    csvWriter.goToNextRow()
-
-    // TODO: Make sure streams are closed if exception is thrown
-    writer.close()
-
-    output.close()
+      writer.close()
+    } finally {
+      output.close()
+    }
   }
 
 }
